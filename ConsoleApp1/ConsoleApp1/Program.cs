8baa6462d5a6ab34f4ddad8e8391359dc792bf40
Hua Sheng <xiaoyong6906@126.com>
2018-12-05 23:33:05 +0800

    [.NET] 添加.NET学习基础项目

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BubbleSort
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arrayList = new int[5] {2, 7, 6, 8, 3 };
+
+            for (int i = 0; i < arrayList.Length-1; i++)
+            {
+                for (int j = i+1; j < arrayList.Length; j++)
+                {
+                    if (arrayList[i] > arrayList[j])
+                    {
+                        int temp = arrayList[i];
+                        arrayList[i] = arrayList[j];
+                        arrayList[j] = temp;
+                    }
+                }
+            }
+
+            foreach (int array in arrayList)
+            {
+                Console.WriteLine(array);
+            }
+
+        }
+    }
+}
---
+++ ConsoleApp1/IndexDemo/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IndexDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Test test = new Test();
+            test[0] = "cassie";
+            test[1] = "luke";
+
+            Console.WriteLine(test[0]);
+            Console.WriteLine(test[1]);
+            Console.ReadKey();
+        }
+    }
+
+    public class Test
+    {
+        private string[] name = new string[2];
+        public string this[int index]
+        {
+            get
+            {
+                return name[index];
+            }
+            set
+            {
+                name[index] = value;
+            }
+        }
+    }
+}
