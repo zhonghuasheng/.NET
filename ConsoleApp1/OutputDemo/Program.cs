b038fdfd204f6da2883aa467a56eda1456394c09
Hua Sheng <xiaoyong6906@126.com>
2018-12-06 22:58:16 +0800

    [.NET] 测试override方法

---
+++ ConsoleApp1/OutputDemo/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OutputDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            B a = new B();
+            Console.ReadKey();
+        }
+    }
+
+    class A
+    {
+        public A()
+        {
+            Print();
+        }
+
+        public virtual void Print() { }
+    }
+
+    class B : A
+    {
+        int x = -1;
+        int y = 2;
+        public B()
+        {
+            y = 1;
+        }
+
+        public override void Print()
+        {
+            Console.WriteLine("x = {0}, y={1}", x, y);
+        }
+    }
+}
