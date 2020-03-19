f485db7c2b1ca1b4d8926891f6f2efba01d446fd
cassiezhang <xiaoyong6906@126.com>
2020-03-19 22:38:11 +0800

    feat(new function): 运用bootstrap来美化网站首页

---
+++ .NetCore/LM/LM/Migrations/20200319132134_UpdateTable.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace LM.Migrations
+{
+    public partial class UpdateTable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "ShotDescription",
+                table: "Noodles",
+                newName: "ShortDescription");
+
+            migrationBuilder.AlterColumn<bool>(
+                name: "IsInStock",
+                table: "Noodles",
+                nullable: true,
+                oldClrType: typeof(bool));
+
+            migrationBuilder.AddColumn<string>(
+                name: "ImageUrl",
+                table: "Noodles",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "LongDescription",
+                table: "Noodles",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "CreateDateUTC",
+                table: "FeedBacks",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ImageUrl",
+                table: "Noodles");
+
+            migrationBuilder.DropColumn(
+                name: "LongDescription",
+                table: "Noodles");
+
+            migrationBuilder.DropColumn(
+                name: "CreateDateUTC",
+                table: "FeedBacks");
+
+            migrationBuilder.RenameColumn(
+                name: "ShortDescription",
+                table: "Noodles",
+                newName: "ShotDescription");
+
+            migrationBuilder.AlterColumn<bool>(
+                name: "IsInStock",
+                table: "Noodles",
+                nullable: false,
+                oldClrType: typeof(bool),
+                oldNullable: true);
+        }
+    }
+}
---
+++ .NetCore/LM/LM/Migrations/20200319134104_UpdateModelTable.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace LM.Migrations
+{
+    public partial class UpdateModelTable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
