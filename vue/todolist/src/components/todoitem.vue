6c003310918cb3c7ad5e5e738b481fdcd6f45b27
cassiezhang <xiaoyong6906@126.com>
2020-03-29 22:16:23 +0800

    study(vue): 学习前端框架vue并写例子加深印象

---
+++ vue/todolist/src/components/todoitem.vue
@@ -0,0 +1,20 @@
+<template>
+  <li class="item" @click="handleDel">{{content}}</li>
+</template>
+
+<script>
+export default {
+  props:['content','index'],
+  methods:{
+    handleDel(){
+      this.$emit('delete', this.index)
+    }
+  }
+}
+</script>
+
+<style>
+  .item{
+    color: red;
+  }
+</style>
---
+++ vue/todolist/src/todolist.vue
@@ -0,0 +1,38 @@
+<template>
+  <div id="todolist">
+    <div>
+      <input class="item" v-model="inputvalue" />
+      <button @click="handleSubmit">提交</button>
+    </div>
+    <ul>
+      <todo-item v-for="(item, index) in list" :key="index" :content='item' :index='index' @delete='handelDel'></todo-item>
+    </ul>
+  </div>
+</template>
+
+<script>
+import todoitem from './components/todoitem'
+export default {
+  components:{
+    'todo-item': todoitem
+  },
+  data () {
+    return {
+      inputvalue: '',
+      list: []
+    }
+  },
+  methods: {
+      handleSubmit () { 
+        this.list.push(this.inputvalue);
+        this.inputvalue='';
+      },
+      handelDel(index){
+        this.list.splice(index, 1);
+      }
+    }
+}
+</script>
+
+<style>
+</style>
