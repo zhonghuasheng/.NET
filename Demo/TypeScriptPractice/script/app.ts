28ebbef0c3571f98496ac841f2fc9f8076b71e5a
cassiezhang <xiaoyong6906@126.com>
2020-04-05 20:56:09 +0800

    文件归类

--- Demo/TypeScriptPractice/script/app.ts
+++
@@ -1,8 +0,0 @@
-{
-    function sayHello()
-    {
-        const compiler = (document.getElementById('cmopiler') as HTMLInputElement).value;
-        const framework = (document.getElementById('framework') as HTMLInputElement).value;
-        return `Hello ${compiler} and ${framework}`;
-    }
-}
